48d6ce3f1f2945d875a908520baf1fbb9d652805
ShiCheng <1041039835@qq.com>
2024-10-17 16:04:25 +0800

    test: Pass move command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -11,6 +11,37 @@ namespace adas{
         return new(std::nothrow) ExecutorImpl(pose);
     }
     void ExecutorImpl::Execute(const std::string &commands) noexcept {
+        for(const auto cmd : commands)
+        {
+            //如果是M指令
+            if(cmd == 'M')
+            {
+                //根据当前姿态决定如何移动小车
+                if(pose.heading == 'E') ++pose.x;
+                else if(pose.heading == 'W') --pose.x;
+                else if(pose.heading == 'N') ++pose.y;
+                else if(pose.heading == 'S') --pose.y;
+            }
 
+            if(cmd == 'L')
+            {
+                if(pose.heading == 'E') pose.heading = 'N';
+                else if(pose.heading == 'N') pose.heading = 'W';
+                else if(pose.heading == 'W') pose.heading = 'S';
+                else if(pose.heading == 'S') pose.heading = 'E';
+            }
+
+            if (cmd == 'R')
+            {
+                if (pose.heading == 'E')
+                    pose.heading = 'S';
+                else if (pose.heading == 'S')
+                    pose.heading = 'W';
+                else if (pose.heading == 'W')
+                    pose.heading = 'N';
+                else if (pose.heading == 'N')
+                    pose.heading = 'E';
+            }
+        }
     }
 }
