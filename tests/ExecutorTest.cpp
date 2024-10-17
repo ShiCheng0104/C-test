c23391d3ecc95395327200a1824d6c390d1e4410
ShiCheng <1041039835@qq.com>
2024-10-17 16:47:15 +0800

    test: Pass ALL command test cases

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -41,6 +41,116 @@ namespace adas
         const Pose target({1, 0, 'E'});
         ASSERT_EQ(target, executor->Query());
     }
+    TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("M");
+        const Pose target({-1, 0, 'W'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("M");
+        const Pose target({0, 1, 'N'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("M");
+        const Pose target({0, -1, 'S'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_N_given_command_is_L_and_facing_is_E)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("L");
+        const Pose target({0, 0, 'N'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_W_given_command_is_L_and_facing_is_N)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("L");
+        const Pose target({0, 0, 'W'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_S_given_command_is_L_and_facing_is_W)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("L");
+        const Pose target({0, 0, 'S'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_E_given_command_is_L_and_facing_is_S)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("L");
+        const Pose target({0, 0, 'E'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_S_given_command_is_R_and_facing_is_E)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("R");
+        const Pose target({0, 0, 'S'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_W_given_command_is_R_and_facing_is_S)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("R");
+        const Pose target({0, 0, 'W'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_N_given_command_is_R_and_facing_is_W)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("R");
+        const Pose target({0, 0, 'N'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_E_given_command_is_R_and_facing_is_N)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'})); // 给了初始姿势
+        // when
+
+        // then
+        executor->Execute("R");
+        const Pose target({0, 0, 'E'});
+        ASSERT_EQ(target, executor->Query());
+    }
 
 } // namespace adas
 
